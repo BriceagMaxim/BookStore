e1acc518ee51a95b6374ac47c3336146471705c3
Maxim <briceagmaxim@gmail.com>
2021-10-01 18:34:05 +0300

    Added tools for migrate in program class

--- BookStore.API/Program.cs
+++ BookStore.API/Program.cs
@@ -2,8 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.Core.Entities.Identity;
+using BookStore.Persistance.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +16,30 @@ namespace BookStore.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var Services = scope.ServiceProvider;
+                var loggerFactory = Services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = Services.GetRequiredService<BookStoreContext>();
+                    await context.Database.EnsureCreatedAsync();
+                    await context.Database.MigrateAsync();
+
+                    var identityContext = Services.GetRequiredService<IdentityContext>();
+                    await identityContext.Database.EnsureCreatedAsync();
+                    await identityContext.Database.MigrateAsync();
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occured during migration");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
